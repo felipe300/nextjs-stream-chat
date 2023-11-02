5fd0bbbea96e79b4473dcb1311ed5d8d1e954828
Felipe300 <caracoloro@gmail.com>
2023-11-02 16:21:28 -0300

    feat: add chat page channels & user first initialization

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,58 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { useUser } from "@clerk/nextjs";
+import {
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  Chat,
+  LoadingIndicator,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import useInitializeChatClient from "./useInitializeChatClient";
+import { type } from "os";
 
 export default function ChatPage() {
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>Chat page</h2>
-      <UserButton afterSignOutUrl="/" />
+    <div className="h-screen">
+      <Chat client={chatClient}>
+        <div className="flex h-full flex-row">
+          <div className="w-full max-w-[360px]">
+            <ChannelList
+              filters={{
+                type: "messaging",
+                members: { $in: [user.id] },
+              }}
+              sort={{ last_message_at: -1 }}
+              options={{ state: true, presence: true, limit: 10 }}
+            />
+          </div>
+          <div className="h-full w-full">
+            <Channel>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput />
+              </Window>
+              <Thread />
+            </Channel>
+          </div>
+        </div>
+      </Chat>
+      {/* <UserButton afterSignOutUrl="/" /> */}
     </div>
   );
 }
---
+++ src/app/chat/useInitializeChatClient.ts
@@ -0,0 +1,41 @@
+import { useUser } from "@clerk/nextjs";
+import { env } from "@/env";
+import { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+
+export default function useInitializeChatClient() {
+  const { user } = useUser();
+  const [chatClient, setChatClient] = useState<StreamChat | null>(null);
+
+  useEffect(() => {
+    if (!user?.id) return;
+
+    const client = StreamChat.getInstance(env.NEXT_PUBLIC_STREAM_KEY);
+
+    client
+      .connectUser(
+        { id: user.id, name: user.fullName || user.id, image: user.imageUrl },
+        async () => {
+          const response = await fetch("/api/get-token");
+          if (!response.ok) {
+            throw Error("Failed to get token");
+          }
+
+          const body = await response.json();
+          return body.token;
+        },
+      )
+      .catch((err) => console.log("Failed to connect user", err))
+      .then(() => setChatClient(client));
+
+    return () => {
+      setChatClient(null);
+      client
+        .disconnectUser()
+        .catch((err) => console.log("Failed to disconnect user", err))
+        .then(() => console.log("Connection closed"));
+    };
+  }, [user?.id, user?.fullName, user?.imageUrl]);
+
+  return chatClient;
+}
